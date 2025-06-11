0f137f0a298d0ae1d407b90fae9fc05d12e9812d
ZombieFreak115 <knudsenmarius@gmail.com>
2025-06-11 14:44:22 +0200

    some changes

--- ConstructorDestructorTest/ConstructorDestructorTest.cpp
+++ ConstructorDestructorTest/ConstructorDestructorTest.cpp
@@ -4,8 +4,13 @@
 
 
 void createPerson(int grades) {
-    // create person, once out of scope the destructor will be run and the underlying array will be deleted
+    // create person on stack, once out of scope the destructor will be run and the underlying array will be deleted
     Person p = Person(grades);
+}
+
+void createPersonHeap(int grades) {
+    // create person on heap, once out of the scope destructor will NOT be run, as it is a raw pointer.
+    Person* p = new Person(grades);
 }
 
 int main()
@@ -13,6 +18,7 @@ int main()
     int grades = 5;
     std::cout << "Start of program\n";
     createPerson(grades); 
+    createPersonHeap(grades);
     std::cout << "End of program\n";
 }
 
